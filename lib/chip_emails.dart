15cbcbc33ecbf09512a4d621cd03ad3856554c54
Muhammad Omar <48818114+umarKhan1@users.noreply.github.com>
2022-04-21 13:48:18 +0500

    push

---
+++ lib/chip_emails.dart
@@ -0,0 +1,93 @@
+library chip_emails;
+
+import 'package:flutter/material.dart';
+
+/// A Calculator.
+// ignore: must_be_immutable
+class EmailChips extends StatefulWidget {
+  List<String> emails = [];
+  TextEditingController editingController;
+  EmailChips({Key? key, required this.emails, required this.editingController})
+      : super(key: key);
+
+  @override
+  State<EmailChips> createState() => _EmailChipsState();
+}
+
+class _EmailChipsState extends State<EmailChips> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+              child: Center(
+            child: SizedBox(
+              height: 40,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  ...widget.emails
+                      .map(
+                        (email) => Chip(
+                          label: Text(email),
+                          onDeleted: () => {
+                            setState(() {
+                              widget.emails
+                                  .removeWhere((element) => email == element);
+                            })
+                          },
+                        ),
+                      )
+                      .toList(),
+                ],
+              ),
+            ),
+          )),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 50,
+            width: 300,
+            child: TextField(
+              decoration:
+                  const InputDecoration.collapsed(hintText: 'Invite By Email'),
+              controller: widget.editingController,
+              onChanged: (String val) {
+                if (val.endsWith(' ')) {
+                  setState(() {
+                    widget.emails.add(widget.editingController.text);
+                    widget.editingController.text = '';
+                  });
+                }
+              },
+              onEditingComplete: () {
+                setState(() {
+                  widget.emails.add(widget.editingController.text);
+                  widget.editingController.text = '';
+                });
+              },
+            ),
+          ),
+          ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.black)),
+              onPressed: () {},
+              child: Container(
+                height: 50,
+                width: 100,
+                color: Colors.black,
+                child: const Center(
+                    child: Text(
+                  "Show Emails",
+                  style: TextStyle(color: Colors.white),
+                )),
+              ))
+        ],
+      ),
+    );
+  }
+}
